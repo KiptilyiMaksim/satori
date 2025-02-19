ab76660d62b2b5c23653396605a317f664c5ae6a
Киптилый Максим <127656306+KiptilyiMaksim@users.noreply.github.com>
2025-02-19 15:16:44 +0200

    block hero + adaptive 3.0

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+// Header Scroll
+window.addEventListener('scroll', () => {
+	document.querySelector('.header').classList.toggle('scrolled', window.scrollY > 50);
+});
+// Burger Menu
 let menuBtn = document.querySelector('.menu-icon');
 let menu = document.querySelector('.header-menu');
 
 menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-})
+});
