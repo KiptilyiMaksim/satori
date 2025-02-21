5776200bfa7bb77571f990625abcacfd38549911
Киптилый Максим <127656306+KiptilyiMaksim@users.noreply.github.com>
2025-02-21 14:30:13 +0200

    fix v1 21.02

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ window.addEventListener('scroll', () => {
 	document.querySelector('.header').classList.toggle('scrolled', window.scrollY > 50);
 });
 // Burger Menu
-let menuBtn = document.querySelector('.menu-icon');
-let menu = document.querySelector('.header-menu');
+let menuBtn = document.querySelector('.burger-menu__icon');
+let menu = document.querySelector('.header__menu');
 
 menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
