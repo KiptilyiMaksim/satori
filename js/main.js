77e3fe1b05b84b0f583103e16ec75917a83c7acb
Киптилый Максим <127656306+KiptilyiMaksim@users.noreply.github.com>
2025-02-25 12:57:02 +0200

    add new block "About"
    Сверстал блок About + адаптив + сделал анимацию выезжающего текста в блоке About при первом скролле экрана.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 });
+// Animated
+const elements = document.querySelectorAll('.animated__block');
+const options = {
+	root: null,
+	rootMargin: '0px',
+	threshold: .4
+}
+const callbacks = (entries) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animated');
+		}
+	});
+}
+let observer = new IntersectionObserver(callbacks, options);
+elements.forEach(element => {
+	observer.observe(element);
+});
