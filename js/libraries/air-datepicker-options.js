97ba2c311e09c1c38cd5c54a6d37e433b7c284c4
Киптилый Максим <127656306+KiptilyiMaksim@users.noreply.github.com>
2025-06-12 16:56:18 +0300

    add file air-datepicker-options.js

---
+++ js/libraries/air-datepicker-options.js
@@ -0,0 +1,32 @@
+let dpMin = new AirDatepicker('#air-datepicker-1', {
+	locale: {
+		days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+		months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	},
+	dateFormat: 'dd/MM/yyyy',
+	keyboardNav: true,
+	onSelect({ date }) {
+		dpMax.update({
+			minDate: date
+		});
+	}
+});
+let dpMax = new AirDatepicker('#air-datepicker-2', {
+	locale: {
+		days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+		months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	},
+	dateFormat: 'dd/MM/yyyy',
+	keyboardNav: true,
+	onSelect({ date }) {
+		dpMin.update({
+			maxDate: date
+		});
+	}
+});
